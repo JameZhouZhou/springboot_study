052f750d2f8606083e18fc39f6714fa459829273
MinisterZ <1289411876@qq.com>
2021-10-20 23:05:42 +0800

    更新了logging

---
+++ spring-boot-03-logging/src/main/java/com/zxy/SpringBootApplication.java
@@ -0,0 +1,15 @@
+package com.zxy;
+
+import org.springframework.boot.SpringApplication;
+
+/**
+ * @version JDK  1.8.151
+ * @Author: Mirrors
+ * @Description: Chengdu City
+ */
+@org.springframework.boot.autoconfigure.SpringBootApplication
+public class SpringBootApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootApplication.class, args);
+    }
+}
